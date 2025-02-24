08ed53a76a1a670edcbdaa96f4b07ebccf5a8364
nyambayar <nyambayar2014@gmail.com>
2025-02-25 07:38:04 +0800

    Code complete

---
+++ test.cpp
@@ -0,0 +1,12 @@
+class class_name {
+    access_specifier 1:
+    member1;
+    access_specifier 2:
+    member2;
+    ...
+} object_names;
+
+class computer {...};
+computer laptop1, laptop2;
+
+void Date::init(int dd, int mm, int yy) {...}
